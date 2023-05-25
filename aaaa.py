c31cb69ae5db8260e9c2133f5161fa176f80e096
ugwuejeh <ugwudavidejeh@gmail.com>
2023-05-25 06:53:48 +0100

    Update aaaa.py

--- aaaa.py
+++ aaaa.py
@@ -1,23 +1,28 @@
-# Build an application that extracts phone number, email and link from a website
+# Build an application that extracts phone number, email and link from a website and stores them in a separate file
 import re
 import requests
 import csv
 
 
-mytext = input("Which web page do you want to scrape? :")
+
+Url = input("Which web page do you want to scrape? :")
 # use requests module to access the site
-response = requests.get(mytext)
-#output it in the form of a text
+response = requests.get(Url)
+# convert response to text
 my_text_url = response.text
 
+# we use regular expressions to extract the phone number, email, and link.
+scrape =r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|\b(?:https?://|www\.)\S+\b|\b(?:\+\d{1,3}\s)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\b"
 
-To_scrape = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b|\b(?:https?://|www\.)\S+\b|\b(?:\+\d{1,3}\s)?\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}\b"
+Data = re.findall(scrape, my_text_url)
 
-matches = re.findall(To_scrape, my_text_url)
 
-with open('matches.csv', 'w', newline='') as file:
+
+
+# We output the extracted data to a csv file
+with open('Data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(['Matches'])
-    writer.writerows([[match] for match in matches])
+    writer.writerow(['Data'])
+    writer.writerows([[data] for data in Data])
 
-print("Completed,your file can be found at'matches.csv'.")
+print("Completed,your file can be found at'Data.csv'.")
